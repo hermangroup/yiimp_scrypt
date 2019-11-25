aa55049dc7215860d98cfee9da38233d09e921b5
Herman Freeman <33547230+hermangroup@users.noreply.github.com>
2019-11-25 16:10:51 +0330

    Updated with x16rv2 + x21s

--- screen-stratum.sh
+++ screen-stratum.sh
@@ -2,7 +2,9 @@
  STRATUM_DIR=/var/stratum
  
  screen -dmS x16r $STRATUM_DIR/run.sh x16r
+ screen -dmS x16r $STRATUM_DIR/run.sh x16rv2
  screen -dmS x16s $STRATUM_DIR/run.sh x16s
+ screen -dmS x16s $STRATUM_DIR/run.sh x21s
  screen -dmS scrypt $STRATUM_DIR/run.sh scrypt
 
 
