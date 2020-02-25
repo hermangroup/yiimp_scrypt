94323169096b0a916b4fdb4ea739a5c2983e4c18
Herman Freeman <33547230+hermangroup@users.noreply.github.com>
2020-02-25 17:02:38 +0330

    Updated with x16rv2 + x21s + x25x

--- screen-stratum.sh
+++ screen-stratum.sh
@@ -5,6 +5,7 @@
  screen -dmS x16r $STRATUM_DIR/run.sh x16rv2
  screen -dmS x16s $STRATUM_DIR/run.sh x16s
  screen -dmS x16s $STRATUM_DIR/run.sh x21s
+ screen -dmS x16s $STRATUM_DIR/run.sh x25x
  screen -dmS scrypt $STRATUM_DIR/run.sh scrypt
 
 
