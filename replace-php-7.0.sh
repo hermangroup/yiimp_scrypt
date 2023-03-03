b34662dcac15069bd33a42451ca3645713060b0a
Herman Freeman <33547230+hermangroup@users.noreply.github.com>
2023-03-03 16:14:40 +0330

    Create replace-php-7.0.sh

---
+++ replace-php-7.0.sh
@@ -0,0 +1,5 @@
+sudo add-apt-repository ppa:ondrej/php
+
+sudo apt-get update
+
+sudo apt-get install php7.0 php7.0-cli php7.0-common php7.0-json php7.0-opcache php7.0-mysql php7.0-mbstring php7.0-mcrypt php7.0-zip php7.0-fpm
